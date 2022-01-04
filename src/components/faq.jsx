5617bc19b22ddc8ed80cbcf50ff8d6ff6fed8b0c
4ix <jinjihoon@gmail.com>
2022-01-04 10:23:51 +0900

    chagne faq

--- src/components/faq.jsx
+++ src/components/faq.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import Accordion from './Accordion';
+import { accordionData } from '../data/content';
+
 export const Faqs = (props) => {
   return (
     <div id='faqs'>
@@ -5,24 +9,11 @@ export const Faqs = (props) => {
         <div className='section-title text-center'>
           <h2>FAQ</h2>
         </div>
-        <div className='row'>
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-md-4'>
-                  <div className='faq'>
-                    <div className='faq-image'>
-                      {' '}
-                      <img src={d.img} alt='' />{' '}
-                    </div>
-                    <div className='faq-content'>
-                      <p>"{d.text}"</p>
-                      <div className='faq-meta'> - {d.name} </div>
-                    </div>
-                  </div>
-                </div>
-              ))
-            : 'loading'}
-        </div>
+        <div className="accordion">
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content} />
+        ))}
+      </div>
       </div>
     </div>
   )
